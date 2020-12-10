7d03a3aa7e490e9d043bf5e32a15bdcdc69b5ba4
Gabriela Braga <bragaga@csc-scc.gc.ca>
2020-12-09 22:38:56 -0500

    route a request

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const http = require('http');
 
 const server =  http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    // console.log(req.url, req.method, req.headers);
+
+    // parse the url
+    const url = req.url;
+    if (url === '/') {
+        res.write(`
+        <html>
+            <head><title>Enter Message</title></head>
+            <body>
+                <form action="/message" method="POST">
+                    <input type="text" name="someData">
+                    <button type="submit">Send</button>
+                </form>
+            </body>
+        </html>`);
+    return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write(`
     <html>
         <head><title>hey</title></head>
         <body><h1>hey</h1></body>
     </html>`);
-    res.end();
+    res.end(); 
 });
 
 server.listen(3000);
