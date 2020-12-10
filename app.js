7688c7103ee7fdb0de5ebe343711e7b5911fdb49
Gabriela Braga <bragaga@csc-scc.gc.ca>
2020-12-09 21:43:46 -0500

    log a request and send back a simple response

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server =  http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`
+    <html>
+        <head><title>hey</title></head>
+        <body><h1>hey</h1></body>
+    </html>`);
+    res.end();
 });
 
 server.listen(3000);
